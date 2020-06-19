758c9e2b3a06599d961843610cc897d588fb46e8
Pongsiri Chatkaew <pongsiri.ch@inet.co.th>
2020-06-19 21:36:44 +0700

    'test'

--- app_solution.js
+++
@@ -1,8 +0,0 @@
-const btnDontPush = document.getElementById('btnDontPush')
-btnDontPush.addEventListener('click', function () {
-  const clicked = () => {
-    this.textContent = 'Why you clicked me 😑'
-    this.className = 'big-button-red'
-  }
-  clicked()
-})
